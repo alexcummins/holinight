0fb63e509d4aba3cfac8400e4d4920413446d2f7
TerenceHernandez <t.na.m.hernandez@gmail.com>
2019-01-26 21:16:23 +0000

    Added HostOrJoin

---
+++ app/src/main/java/uk/com/holinight/HostOrJoin.java
@@ -0,0 +1,13 @@
+package uk.com.holinight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HostOrJoin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_host_or_join);
+    }
+}

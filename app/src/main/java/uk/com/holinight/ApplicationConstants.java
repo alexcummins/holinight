9decfc5c6307acfe86cf9157c68e90c2772bfc0f
raghav <rk4718@ic.ac.uk>
2019-01-26 20:03:13 +0000

    updated login feature

---
+++ app/src/main/java/uk/com/holinight/ApplicationConstants.java
@@ -0,0 +1,10 @@
+package uk.com.holinight;
+
+
+
+public interface ApplicationConstants {
+
+   public static final String BASE_URL = "https://www.google.com/";
+
+}
+
--- app/src/main/java/uk/com/holinight/LoginActivity.java
+++ app/src/main/java/uk/com/holinight/LoginActivity.java
@@ -1,6 +1,11 @@
 package uk.com.holinight;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +21,16 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class LoginActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
@@ -51,7 +66,7 @@ public class LoginActivity extends AppCompatActivity {
   public void loginButton(View v) {
         String email = emailField.getText().toString();
         String password = passwordField.getText().toString();
-        updateUI();
+      verifyLogin(email, password);
 //    mAuth
 //        .signInWithEmailAndPassword(email, password)
 //        .addOnCompleteListener(
@@ -81,6 +96,114 @@ public class LoginActivity extends AppCompatActivity {
     private void updateUI(FirebaseUser user) {
         Log.d(TAG, "update screen");
     }
+
+
+    public void verifyLogin(final String email, final String password) {
+
+        class VerifyLoginAsyncTask extends AsyncTask<Void, Void, String> {
+
+            ProgressDialog dialog = ProgressDialog.show(LoginActivity.this, "",
+                    "", true);
+
+            @Override
+            protected void onPostExecute(String result) {
+                super.onPostExecute(result);
+                dialog.cancel();
+                if (result != null) {
+                    try {
+                        JSONObject items = new JSONObject(result);
+                        int value = items.getInt("value");
+                        if (value == 0) {
+                            Toast.makeText(LoginActivity.this,
+                                    "Please enter correct e-mail or password!", Toast.LENGTH_LONG).show();
+                        } else {
+
+                            JSONArray userDataArray = items.getJSONArray("users_data");
+                            JSONObject userDataJsonObject = userDataArray
+                                    .getJSONObject(0);
+
+
+                            Log.d(TAG, "email "+userDataJsonObject.getString("email"));
+
+                            SharedPreferences prefs = PreferenceManager
+                                    .getDefaultSharedPreferences(LoginActivity.this);
+                            SharedPreferences.Editor ed = prefs.edit();
+
+                            }
+
+
+                            EditText email = (EditText) findViewById(R.id.email_text);
+                            email.setText("");
+                            EditText password = (EditText) findViewById(R.id.password_text);
+                            password.setText("");
+                        updateUI();
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            protected String doInBackground(Void... params) {
+                StringBuilder responseStr = new StringBuilder();
+                try {
+                    String url = ApplicationConstants.BASE_URL;
+//                            + email + "&password="
+//                            + Uri.encode(password);
+                    Log.d(TAG, url);
+                    HttpGet httpGet = new HttpGet(url);
+
+                    HttpResponse response = HttpConnectionManager.getClient()
+                            .execute(httpGet);
+
+                    InputStream responseInputStream = response.getEntity()
+                            .getContent();
+                    BufferedReader bufferedReader = new BufferedReader(
+                            new InputStreamReader(responseInputStream));
+
+                    String responseLineStr = null;
+                    while ((responseLineStr = bufferedReader.readLine()) != null) {
+                        responseStr.append(responseLineStr);
+
+                    }
+
+                    bufferedReader.close();
+                    responseInputStream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+
+                }
+
+                return responseStr.toString();
+            }
+
+        }
+
+        VerifyLoginAsyncTask verifyLoginAsyncTask = new VerifyLoginAsyncTask();
+        verifyLoginAsyncTask.execute((Void) null);
+    }
+
+    public void forgotPasswordButton(View view) {
+        Intent mainIntent = new Intent(LoginActivity.this,
+                SwipeSearch.class);
+       // mainIntent.putExtras(bundleData);
+        startActivity(mainIntent);
+    }
+
+    public static String bundle2string(Bundle bundle) {
+        if (bundle == null) {
+            return null;
+        }
+        String string = "Bundle{";
+        for (String key : bundle.keySet()) {
+            string += " " + key + " => " + bundle.get(key) + ";";
+        }
+        string += " }Bundle";
+        return string;
+    }
+
+
 
     private  void updateUI(){
         Intent nextActivity = new Intent(this, SwipeSearch.class);

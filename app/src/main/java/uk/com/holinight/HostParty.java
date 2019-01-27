57144a03d94a7797a6cd4fcabaf68ea3ae023b20
TerenceHernandez <t.na.m.hernandez@gmail.com>
2019-01-27 01:57:26 +0000

    Added "HostParty" Activity

---
+++ app/src/main/java/uk/com/holinight/HostParty.java
@@ -0,0 +1,15 @@
+package uk.com.holinight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HostParty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_host_party);
+    }
+
+
+}

af5f6c0e22271ec131acd9790dfbdb1b690fe07d
kk44838 <kazuya.kaiolowu@gmail.com>
2019-01-27 08:13:38 +0000

    JASON

--- app/src/main/java/uk/com/holinight/HostParty.java
+++ app/src/main/java/uk/com/holinight/HostParty.java
@@ -2,13 +2,19 @@ package uk.com.holinight;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class HostParty extends AppCompatActivity {
+
+    private EditText descriptionField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_host_party);
+
+        descriptionField = (EditText) findViewById(R.id.description);
+
     }
 
 

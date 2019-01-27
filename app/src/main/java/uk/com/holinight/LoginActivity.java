a88b2828f7692bf38edf32579c41aa50f1b11a54
raghav <rk4718@ic.ac.uk>
2019-01-27 00:33:28 +0000

    json parser

--- app/src/main/java/uk/com/holinight/LoginActivity.java
+++ app/src/main/java/uk/com/holinight/LoginActivity.java
@@ -57,6 +57,7 @@ public class LoginActivity extends AppCompatActivity {
     //         Check if user is signed in (non-null) and update UI accordingly.
     FirebaseUser currentUser = mAuth.getCurrentUser();
     if (currentUser != null) {
+      Log.d(TAG, currentUser.getUid());
       updateUI(currentUser);
     }
   }
